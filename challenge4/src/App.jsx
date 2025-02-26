341fa81573fae6a5e184da000fca5d514dfd738c
josereyna07 <josedaniel3046@gmail.com>
2025-02-26 13:04:32 -0500

    Actualización con nuevos cambios

--- challenge4/src/App.jsx
+++ challenge4/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import CategoryInput from "./assets/CategoryInput";
+import CategoryInput from "./CategoryInput";
+
 
 function App() {
   const [categories, setCategories] = useState([]);
